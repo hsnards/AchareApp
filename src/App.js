da53c08baae726c8cc95e5ab20e86df6e0c56e52
hsnards <1214hassan1214@gmail.com>
2022-02-13 09:43:29 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import "./App.css";
+import CompleteInformation from "./components/CompleteInformation";
+import ChooseLocation from './components/ChooseLocation';
+import ViewAllAddresses from './components/ViewAllAddress';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [showForm , setShowForm] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={showForm  ? <CompleteInformation /> : <ChooseLocation />} />
+        <Route exact path="/all-addresses" element={<ViewAllAddresses />} />
+      </Routes>
     </div>
   );
 }
